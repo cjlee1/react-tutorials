0091c28d20d58163e955bb1c3c5a73f6f72f2b14
cjlee1 <cjlee@ualberta.ca>
2021-07-14 15:51:00 -0600

    set count

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState,useEffect} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function expensiveInitialState(){
+    return 10;
 }
 
+const App = () => {
+
+   
+    // const [{count,count2},setCount] =  useState({count:10,count2:20});
+    const [count,setCount] =  useState(10);
+    const [count2,setCount2] =  useState(20);
+
+    return (<div>
+        
+            <div>
+                count 1: {count}
+                
+            </div>
+            <div>
+            count 2: {count2}
+
+            </div>
+        <button 
+            onClick={()=>
+            {
+                setCount(c =>c+1);
+                setCount2(c =>c+1);
+            }
+            }>+</button>
+        <input name="email"/>
+        <input name="password" type="password" />
+
+    </div>);
+}
 export default App;
