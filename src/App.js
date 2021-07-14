6d59bc00c782425db4456fe92219b689422007a8
cjlee1 <cjlee@ualberta.ca>
2021-07-14 16:17:55 -0600

    finished usedState Tutorial

--- src/App.js
+++ src/App.js
@@ -7,23 +7,23 @@ function expensiveInitialState() {
 
 const App = () => {
 	// const [{count,count2},setCount] =  useState({count:10,count2:20});
-	const [ values, handleChange ] = useForm({email:'',password:''});
-	const [ values2, handleChange2 ] = useForm({firstName:'',lastName:''});
+	const [ values, handleChange ] = useForm({ email: '', password: '' });
+	const [ values2, handleChange2 ] = useForm({ firstName: '', lastName: '' });
 
 	return (
 		<div>
-			
-			<input name="email"  value = {values.email} onChange={handleChange}/>
-			<input name="password" type="password" value = {values.password} onChange={handleChange}/>
-            <input name="firstName"  value = {values2.firstName} onChange={handleChange2}/>
-			<input name="lastName"  value = {values2.lastName} onChange={handleChange2}/>
-            <div>
-            <p>Your email is: {values.email}</p>
-       <p>    your pass is: {values.password}</p>
-       <p>Your name is: {values2.firstName} {values2.lastName}</p>
-        </div>
-        </div>
-       
+			<input name="email" value={values.email} onChange={handleChange} />
+			<input name="password" type="password" value={values.password} onChange={handleChange} />
+			<input name="firstName" value={values2.firstName} onChange={handleChange2} />
+			<input name="lastName" value={values2.lastName} onChange={handleChange2} />
+			<div>
+				<p>Your email is: {values.email}</p>
+				<p> your pass is: {values.password}</p>
+				<p>
+					Your name is: {values2.firstName} {values2.lastName}
+				</p>
+			</div>
+		</div>
 	);
 };
 export default App;
