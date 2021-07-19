043e7e40372f75d855f1afc6fda90d3e7408cb68
cjlee1 <cjlee@ualberta.ca>
2021-07-19 16:27:39 -0600

    useReducer start

--- src/App.js
+++ src/App.js
@@ -1,52 +1,33 @@
-import React, { useState,useCallback, useEffect, useMemo} from 'react';
+import React, {useReducer} from 'react';
 // import { Hello } from './Hello';
 // import { Square } from './Square';
 import {useFetch} from './useFetch';
 
-
+// alternative to useState hook
+function reducer(state,action){
+	switch(action,type){
+		case 'INCREMENT':
+			return state + 1
+		case 'DECREMENT':
+			return state - 1
+		default:
+			return state;
+	}
+}
 
 const App = () => {
-	const [count,setCount] = useState(0);
-	const favoriteNums = [7,21,37];
-	//prevent function from changing the value, like when using react.memo or useeffect
-	// const increment = useCallback((n)=>{
-	// 	setCount(c=>c +n);
-	// },[setCount]);
-
-	const {data} = useFetch('https://raw.githubusercontent.com/ajzbc/kanye.rest/master/quotes.json')
-
-	const computeLongestWord = useCallback(arr=>{
-
-		if(!arr){
-			return [];
-		}
-		console.log("computing longest word")
-		let longestWord = '';
-		JSON.parse(arr).forEach(sentence => sentence.split(" ").forEach(word =>{
-			if(word.length > longestWord.length){
-				longestWord=word;
-			}
-		})
-		);
-		return longestWord;
-	},[]);
-
-	const longestWord = useMemo(()=> computeLongestWord(data),[data]);
+	const [count,dispatch ] = useReducer(reducer,0);
 	return (
-		<div>
+		<div>  
 		
-			 {/* <Hello increment ={increment} /> */}
-			 <div> count : {count}</div>
-			{/* {favoriteNums.map(n=>{
-				return (
-					// <Square increment ={increment}  n={n} key ={n}/>
+			<div> count: { count}</div>
+			<button onClick={()=>dispatch(
+				{type:"INCREMENT"}
+				)}>increment</button>
+			<button onClick={()=>dispatch({
+				type:"DECREMENT"}
+				)}>decrement</button>
 
-				);
-			})} */}
-			<button onClick={()=> setCount(count + 1)} > + </button>
-			<div>
-				{longestWord}
-			</div>
 		</div>
 	);
 };
